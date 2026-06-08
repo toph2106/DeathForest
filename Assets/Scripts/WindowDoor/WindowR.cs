1f9df71d2d21afba2f6f71789b65d0516c78d0dd
manhn2006 <sd2102006@gmail.com>
2026-06-08 17:11:59 +0700

    Map00 lan 1

---
+++ Assets/Scripts/WindowDoor/WindowR.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+
+public class WindowR : MonoBehaviour, IInteractable
+{
+    public float slideDistance = 1.5f;
+    public float slideSpeed = 5f;
+
+    private Vector3 closedPos;
+    private Vector3 openPos;
+    private Vector3 targetPos;
+    private bool isOpen = false;
+
+    void Start()
+    {
+        closedPos = transform.position;
+
+        openPos = closedPos + (transform.right * slideDistance);
+
+        targetPos = closedPos;
+    }
+
+    void Update()
+    {
+        transform.position = Vector3.Lerp(transform.position, targetPos, Time.deltaTime * slideSpeed);
+    }
+
+    public void Interact()
+    {
+        isOpen = !isOpen;
+        targetPos = isOpen ? openPos : closedPos;
+    }
+}
